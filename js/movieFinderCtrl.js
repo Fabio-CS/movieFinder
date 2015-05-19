c5055458b51faf35822ad525dcca70fe256bdf75
Fabio Miranda <fmiranda@AL-HOU-fmiranda.local>
2015-05-19 12:40:48 -0500

    Search page tested and finalized

--- js/movieFinderCtrl.js
+++ js/movieFinderCtrl.js
@@ -1,8 +1,10 @@
 movieFinder.controller('movieFinderCtrl', function($scope, $http, $log) {
     $scope.zeroState = true;
-    $scope.currentPage = 1;
     $scope.maxSize = 10;
     $scope.hasPagination = false;
+    $scope.initSearch = function(){
+        $scope.currentPage = 1;
+    }
     $scope.search = function(){
         $scope.hasPagination = false;
         $scope.zeroState = false;
@@ -14,19 +16,31 @@ movieFinder.controller('movieFinderCtrl', function($scope, $http, $log) {
         $scope.movies = [];
         $scope.actors = [];
         $scope.tv = [];
-        $http.get("https://api.themoviedb.org/3/search/multi?api_key=7fcba73e299bdc5db11e6a6121b0457b&query="+ $scope.query +"&search_type=ngram")
+        $http.get("https://api.themoviedb.org/3/search/multi?api_key=7fcba73e299bdc5db11e6a6121b0457b&query="+ $scope.query +"&search_type=ngram&page="+$scope.currentPage)
             .success(function(response) {
                 if(response.total_pages > 1){
                     $scope.hasPagination = true;
                 }
             angular.forEach(response.results, function(value, key) {
                         if(value.media_type === "movie"){
+                            if (value.poster_path != null && angular.isDefined(value.poster_path))
+                                value.poster_path = "http://image.tmdb.org/t/p/w185/" + value.poster_path;
+                            else
+                                value.poster_path = "/img/no_poster_image.jpg";
                             $scope.movies.push(value);
                             $scope.hasMovies = true;
                         }else if(value.media_type === "person"){
+                            if (value.profile_path != null && angular.isDefined(value.profile_path))
+                                value.profile_path = "http://image.tmdb.org/t/p/w185/" + value.profile_path;
+                            else
+                                value.profile_path = "/img/no_profile_image.jpg";
                             $scope.actors.push(value)
                             $scope.hasActors = true;
                         }else{
+                            if (value.poster_path != null && angular.isDefined(value.poster_path))
+                                value.poster_path = "http://image.tmdb.org/t/p/w185/" + value.poster_path;
+                            else
+                                value.poster_path = "/img/no_poster_image.jpg";
                             $scope.tv.push(value);
                             $scope.hasTV = true;
                         }
@@ -41,34 +55,4 @@ movieFinder.controller('movieFinderCtrl', function($scope, $http, $log) {
                 }
             });
         };
-    $scope.pageChanged = function(){
-        $scope.movies = [];
-        $scope.actors = [];
-        $scope.tv = [];
-        $scope.hasMovies = false;
-        $scope.hasActors = false;
-        $scope.hasTV = false;
-        $http.get("https://api.themoviedb.org/3/search/multi?api_key=7fcba73e299bdc5db11e6a6121b0457b&query="+ $scope.query +"&search_type=ngram&page="+$scope.currentPage)
-            .success(function(response) {
-            angular.forEach(response.results, function(value, key) {
-                if(value.media_type === "movie"){
-                    $scope.movies.push(value);
-                    $scope.hasMovies = true;
-                }else if(value.media_type === "person"){
-                    $scope.actors.push(value)
-                    $scope.hasActors = true;
-                }else{
-                      $scope.tv.push(value);
-                      $scope.hasTV = true;
-                }
-            });
-            if($scope.hasMovies && $scope.hasActors && $scope.hasTV){
-                        $scope.colClass = "col-sm-4";
-        }else if(($scope.hasMovies && $scope.hasActors && !$scope.hasTV) || ($scope.hasMovies && !$scope.hasActors && $scope.hasTV) || (!$scope.hasMovies && $scope.hasActors && $scope.hasTV)){
-            $scope.colClass = "col-sm-6";
-        }else if(($scope.hasMovies && !$scope.hasActors && !$scope.hasTV) || (!$scope.hasMovies && !$scope.hasActors && $scope.hasTV) || (!$scope.hasMovies && $scope.hasActors && !$scope.hasTV)){
-            $scope.colClass = "col-sm-12";
-        }
-        });
-    };
 });
