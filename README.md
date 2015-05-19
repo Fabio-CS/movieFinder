5795fffe34602c8c99a8790188806d39598c8438
Fabio Miranda <fmiranda@AL-HOU-fmiranda.local>
2015-05-19 18:38:04 -0500

    Finished development

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # movieFinder
 Repository for Alert Logic developer test task
 
-This is an application which uses the moviedb API to present a search application able to search for actor names and movie names.
+This is an application developed using AngularJS and Bootstrapm which uses the moviedb API to search for actor names, movie names and TV shows. The layout is also mobile-friendly.
--- js/movieFinderCtrl.js
+++ js/movieFinderCtrl.js
@@ -1,15 +1,20 @@
 movieFinder.controller('movieFinderCtrl', function($scope, $http, $log) {
-    $scope.zeroState = true;
+    $scope.hasResults = false;
     $scope.maxSize = 10;
     $scope.hasPagination = false;
+    $scope.currentPage = 1;
     $scope.initSearch = function(){
         $scope.currentPage = 1;
-    }
+        $scope.movies = [];
+        $scope.actors = [];
+        $scope.tv = [];
+    };
     $scope.search = function(){
         $scope.hasPagination = false;
-        $scope.zeroState = false;
+        $scope.hasResults = true;
         $scope.hasMovies = false;
         $scope.hasActors = false;
+        $scope.hasDetails = false;
         $scope.hasTV = false;
         $scope.totalResults = 0;
         $scope.totalPages = 0;
@@ -55,4 +60,26 @@ movieFinder.controller('movieFinderCtrl', function($scope, $http, $log) {
                 }
             });
         };
+    $scope.getInfoDetails = function(type, id){
+        $scope.infoDetail = null;
+        $scope.hasDetails = true;
+        $scope.hasResults = false;
+        $scope.hasPagination = false;
+        $http.get("https://api.themoviedb.org/3/"+type+"/"+id+"?api_key=7fcba73e299bdc5db11e6a6121b0457b")
+            .success(function(response) {
+                if(type === "person"){
+                    if (response.profile_path != null && angular.isDefined(response.profile_path))
+                        response.image = "http://image.tmdb.org/t/p/h632/" + response.profile_path;
+                    else
+                        response.image = "/img/no_profile_image.jpg";
+                }else{
+                    if (response.poster_path != null && angular.isDefined(response.poster_path))
+                        response.image = "http://image.tmdb.org/t/p/w342/" + response.poster_path;
+                    else
+                        response.image = "/img/no_poster_image.jpg";
+                }
+            response.media_type = type;
+            $scope.infoDetail = response;
+        });
+    };
 });
